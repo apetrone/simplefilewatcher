7697558a24f4f34c0a42bd94ecc8c66e28fa57e9
saintonfire <saintonfire@d2862050-3e7c-11de-90e1-f9f1724fc9cc>
2009-09-14 20:45:57 +0000

    A little more to the build script.
    git-svn-id: http://simplefilewatcher.googlecode.com/svn/trunk@7 d2862050-3e7c-11de-90e1-f9f1724fc9cc

--- build.lua
+++ build.lua
@@ -1,49 +1,27 @@
 -- A solution contains projects, and defines the available configurations
 solution "SimpleFileWatcher"
-	location(_ACTION)
+	location("build/" .. _ACTION)
 	configurations { "Debug", "Release" }
 
 	-- A project defines one build target
 	project "SimpleDemo"
 		kind "ConsoleApp"
-		location(_ACTION)
+		location("build/" .. _ACTION)
 		language "C++"
 		files { 
 			"source/*.cpp",
 			"SimpleDemo.cpp"
 		}
 		includedirs { "includes" }
-		objdir "intermediate/SimpleDemo"
-
-	project "OgreDemo"
-		kind "ConsoleApp"
-		location(_ACTION)
-		language "C++"
-		files {
-			"source/*.cpp",
-			"OgreDemo.cpp"
-		}
-		includedirs {
-			"includes",
-			"exampleapp"
-		}
-	
-	configuration {"SimpleDemo", "Release"}
-		objdir "intermediate/SimpleDemo/Release"
-	
-	configuration {"SimpleDemo", "Debug"}
-		objdir "intermediate/SimpleDemo/Debug"
-	
-	configuration {"OgreDemo", "Release"}
-		objdir "intermediate/OgreDemo/Release"
-	
-	configuration {"OgreDemo", "Debug"}
-		objdir "intermediate/OgreDemo/Debug"
+		objdir "build/intermediate"
 	
 	configuration "Debug"
 		defines { "DEBUG" }
 		flags { "Symbols" }
+		objdir "build/intermediate/Debug"
+		targetname "SimpleDemo_d"
 
 	configuration "Release"
 		defines { "NDEBUG" }
 		flags { "Optimize" }
+		objdir "build/intermediate/Release"
