22764a2d983672b3a2fdd399e0b8dc6120a7c728
saintonfire <saintonfire@d2862050-3e7c-11de-90e1-f9f1724fc9cc>
2009-09-15 02:28:46 +0000

    Removed old build scripts. Fixed problem in SimpleDemo.cpp.
    git-svn-id: http://simplefilewatcher.googlecode.com/svn/trunk@9 d2862050-3e7c-11de-90e1-f9f1724fc9cc

--- SimpleDemo.cpp
+++ SimpleDemo.cpp
@@ -59,7 +59,6 @@ int main(int argc, char **argv)
 		{
 			fileWatcher.update();
 		}
-kj
 	} 
 	catch( std::exception& e ) 
 	{
--- build.lua
+++ build.lua
@@ -1,19 +1,19 @@
 -- A solution contains projects, and defines the available configurations
 solution "SimpleFileWatcher"
 	location("build/" .. _ACTION)
+	targetdir "bin"
 	configurations { "Debug", "Release" }
 
 	-- A project defines one build target
 	project "SimpleDemo"
 		kind "ConsoleApp"
-		location("build/" .. _ACTION)
 		language "C++"
 		files { 
 			"source/*.cpp",
 			"SimpleDemo.cpp"
 		}
 		includedirs { "includes" }
-		objdir "build/intermediate"
+		objdir "intermediate"
 	
 	configuration "Debug"
 		defines { "DEBUG" }
